1af342cf66a983d299794a55d31c4b9d600d5c73
vaishnavi-kamde <kamdevaishnavi1995@gmail.com>
2022-12-22 16:44:31 +0530

    Added Part Time Program

--- src/com/brizz/usecase/EmployeeWage.java
+++ src/com/brizz/usecase/EmployeeWage.java
@@ -6,12 +6,18 @@ public class EmployeeWage {
         System.out.println("Welcome to Employee Wage Computation Program");
         final int WAGE_PER_HR = 20;
         final int FULL_DAY = 8;
+        final int PARTTIME = 1;
         int pay = 0;
-        double num = Math.floor((Math.random() * 10)) % 2;
+        double num = Math.floor((Math.random() * 10)) % 3;
+        System.out.println(num);
         if (num == 1) {
             System.out.println("Employee is Present ");
             pay = WAGE_PER_HR * FULL_DAY;
-            System.out.println("Employee paid per day is "+pay);
+            System.out.println("Employee Daily Wage "+pay);
+        }
+        else if (num == 2){
+            System.out.println("Employee is Part-Time");
+            pay = WAGE_PER_HR * PARTTIME;
         }
         else{
             System.out.println("Employee is Absent ");
