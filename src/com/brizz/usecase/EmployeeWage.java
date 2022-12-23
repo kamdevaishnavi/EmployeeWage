98a690c5710f9f91536b4497bf139f360e58f67f
vaishnavi-kamde <kamdevaishnavi1995@gmail.com>
2022-12-23 08:04:26 +0530

    Added Emp Daily Wage Program

--- src/com/brizz/usecase/EmployeeWage.java
+++ src/com/brizz/usecase/EmployeeWage.java
@@ -2,30 +2,33 @@ package com.brizz.usecase;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        System.out.println("Welcome to Employee Wage Computation Program");
-        final int WAGE_PER_HR = 20;
-        final int FULL_DAY = 8;
-        final int PARTTIME = 4;
-        int pay = 0;
-        int num = (int) Math.floor((Math.random() * 10)) % 3;
-        System.out.println(num);
-        switch (num) {
-            case 1: {
-                System.out.println("Employee is Present ");
-                pay = WAGE_PER_HR * FULL_DAY;
+        System.out.println("welcome to employee wage program");
+        int empHrs;
+        int dailyWage;
+        int monthlyWage = 0;
+        int empWagePerHrs = 20;
+        int workingDaysPerMonth = 20;
+        for(int day = 0; day < workingDaysPerMonth; day++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case 1:
+                    System.out.println("Employee is Present");
+                    empHrs= 8;
+                    break;
+                case 2:
+                    System.out.println("Employee is Part-Time Present");
+                    empHrs = 4;
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+                    empHrs= 0;
             }
-               break;
-            case 2: {
-                System.out.println("Employee is Part-Time");
-                pay = WAGE_PER_HR * PARTTIME;
-            }
-              break;
-            case 0: {
-                System.out.println("Employee is Absent ");
-            }
+            dailyWage = empHrs * empWagePerHrs;
+            monthlyWage = dailyWage + monthlyWage;
+            System.out.println("Daily wage of employee is "+ dailyWage);
         }
-        System.out.println("Employee Daily Wage " + pay);
-
+        System.out.println("  ");
+        System.out.println("Monthly employee wage is "+ monthlyWage);
     }
 }
+
