108f620bac25dfe4e156b98690a2c38ec8f1dcd2
vaishnavi-kamde <kamdevaishnavi1995@gmail.com>
2023-01-09 12:34:15 +0530

    ADDED :- refactor the code

--- src/com/bridgelabz/usecases/EmployeeWage.java
+++ src/com/bridgelabz/usecases/EmployeeWage.java
@@ -2,8 +2,7 @@ package com.bridgelabz.usecases;
 
 public class EmployeeWage {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
+    public void calculateWage() {
         final int WAGE_PER_HR = 20;
         final int WORKING_DAYS = 20;
         int max_emp_hr = 100;
@@ -13,8 +12,8 @@ public class EmployeeWage {
 
         int total_emp_hr = 0;
         int total_working_days = 1;
-        int empCheck =0;
-        while(total_emp_hr  <= max_emp_hr && total_working_days  <= WORKING_DAYS){
+        int empCheck = 0;
+        while (total_emp_hr <= max_emp_hr && total_working_days <= WORKING_DAYS) {
 
             empCheck = (int) (Math.floor((Math.random() * 10)) % 3);
             switch (empCheck) {
@@ -31,11 +30,17 @@ public class EmployeeWage {
 
             }
 
-            total_emp_hr = total_emp_hr+emp_hr;
-            System.out.println("Total days "+total_working_days+ " Total working hrs "+total_emp_hr);
+            total_emp_hr = total_emp_hr + emp_hr;
+            System.out.println("Total days " + total_working_days + " Total working hrs " + total_emp_hr);
             total_working_days++;
         }
-        totalEmpWage = total_emp_hr*WAGE_PER_HR;
+        totalEmpWage = total_emp_hr * WAGE_PER_HR;
         System.out.println("Monthly wage is " + totalEmpWage);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+        EmployeeWage obj = new EmployeeWage();
+        obj.calculateWage();
     }
 }
